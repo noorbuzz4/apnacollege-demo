fe91cc2a914bc362e13d217f0d7f1569190e3dea
Kinza Tahir <130205311+Kinzatahir11@users.noreply.github.com>
2024-04-23 11:29:37 +0500

    Github learning paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Learn Github It will Help you to growth <br/>in your development Career
         </p>
         <a
           className="App-link"
